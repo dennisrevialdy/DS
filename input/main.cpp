2052d098877110e8cc9a47acf12cb104e1e28e74
dennisrevialdy <122371567+dennisrevialdy@users.noreply.github.com>
2026-04-29 09:23:55 +0800

    Add files via upload

---
+++ input/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+    int n;
+    int t;
+    std:: cin >> t;
+
+    for(int i =0; i < t; i++){
+        std::cin >> n;
+        for(int j = 0; j < n; j++){
+            std::cout << "Hello World" << std::endl;
+        }
+    }
+    
+    return 0;
+}
